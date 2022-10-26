2b26111cb83ced52b75679073bb6dcd7014ed13a
Hy <88772679+gagagahuy@users.noreply.github.com>
2022-10-26 10:06:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const app = {
         {
             name: '白月光与朱砂痣',
             author: 'Đại Tử',
-            image: './img/AcMa.jfif',
+            image: './image/Suzy2.jpg',
             path: './music/BachNguyetQuangVaNotChuSa.mp3'
         },
         {
